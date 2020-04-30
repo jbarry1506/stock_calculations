23d90a2cf810ba6a3cc08a89d39b1dd942f4f4b6
Jim Barry <jbarry1506@gmail.com>
2020-04-30 19:44:23 -0400

    mutuals working

--- yhoo_demo.py
+++ yhoo_demo.py
@@ -30,6 +30,7 @@ import pprint
 
 
 # get all info for stock
+# TODO - CHANGE FUNCTION TO get_investment_info(symbol)
 def get_stock_info(stock):
     stock_data = yf.Ticker(stock)
     stock_info = stock_data.info
@@ -50,17 +51,24 @@ def above_fifty_day(fifty, bid):
 def fifty_above_twohun(twohun, fifty):
     return fifty > twohun
 
+
+# what was the market value at the last close
+    # stock_info['previousClose']
+
 # trading volume
+    # I think this is in the info
 
 # outstanding shares
 # Share Turnover Ratio = trading volume / average shares
 
-my_stocks = ['msft', 'aapl', 'gild', 'sage', 'mdb', 'flr', 'ntnx', 'trrdx']
+my_stocks = ['msft', 'aapl', 'gild', 'sage', 'mdb', 'flr', 'ntnx']
 principal = ['fxnax', 'jcbux', 'mphrx', 'pgblx', 'trrfx', 'trrax', 'trrbx', 
 'trrgx', 'trrhx', 'trrcx', 'trrjx', 'trrdx', 'trrkx', 'trrmx', 'trrnx', 'trrlx',
 'fxaix', 'fcgax', 'peiqx', 'vftnx', 'aredx', 'ggotx', 'jvtnx', 'flmvx', 'fsccx', 
 'vspmx', 'vsmsx', 'rerfx', 'odvyx', 'vtmgx'
 ]
+
+my_investments = [my_stocks, principal]
 
 # TODO - MAKE THIS A FUNCTION
 for ms in principal:
@@ -76,24 +84,30 @@ for ms in principal:
     high_52 = stock_info['fiftyTwoWeekHigh']
     two_hundred_average = stock_info['twoHundredDayAverage']
     fifty_average = stock_info['fiftyDayAverage']
-    
-    if stock_info['tradeable']:
+    previous_close = stock_info['previousClose']
+
+    print('\n', name)
+    # try to find the bid and ask values
+    try:
         ask = stock_info['ask']
         buy = stock_info['bid']
-        ba_spread = bid_ask_spread(get_stock_info(ms))
-        
-        print('\n', name)
+        ba_spread = bid_ask_spread(stock_info)
         print('The sellers are asking {} for {}'.format(ask, ms))
         print('The buyers are buying at {} for {}'.format(buy, ms))
         print('This puts the bid-ask spread at {}'.format(ba_spread))
-        
         if above_fifty_day(fifty_average, buy) and fifty_above_twohun(two_hundred_average, fifty_average):
             print("The current buy price is above the fifty day average and the fifty is above the two hundred.")
         else:
             print("The fifty day average is {}".format(fifty_average))
             print("The two hundred day average is {}".format(two_hundred_average))
-    else:
-        print("{} is not tradable.".format(name))
-        print("The investment type is {}".format(investment_type))
+    except:
+        print("The bid-ask spread can't be calculated for {}".format(name))
+        if above_fifty_day(fifty_average, previous_close) and fifty_above_twohun(two_hundred_average, fifty_average):
+            print("The previous close is above the fifty day average and the fifty is above the two hundred.")
+        else:
+            print("The fifty day average is {}".format(fifty_average))
+            print("The two hundred day average is {}".format(two_hundred_average))            
+
+    print("The investment type is {}".format(investment_type))
     
 print('got all the data')
