8e9c60f6a793c36feea23fe3fb0e31e62e14d610
Jim Barry <jbarry1506@gmail.com>
2020-04-30 17:27:59 -0400

    stock analysis okay, need to handle non-tradable

--- yhoo_demo.py
+++ yhoo_demo.py
@@ -1,6 +1,9 @@
 import yfinance as yf
 import pprint
 
+# Is the stock liquid
+# TODO:  WRITE STOCK LIQUIDITY FUNC
+
 # microsoft = yf.Ticker('MSFT')
 # apple = yf.Ticker('AAPL')
 # vray = yf.Ticker('VRAY')
@@ -15,41 +18,75 @@ import pprint
 # pprint.pprint(data_gild)
 
 ## Data to find
+
+# 52 week change
+    # stock_info['52WeekChange']
+
+# Ask price (seller offer)
+    # stock_info['ask']
+
+# Bid price (buyer offer)
+    # stock_info['bid']
+
+
 # get all info for stock
 def get_stock_info(stock):
     stock_data = yf.Ticker(stock)
     stock_info = stock_data.info
     return stock_info
 
-# Is the stock liquid
-# TODO:  WRITE STOCK LIQUIDITY FUNC
-
-# Ask price (seller offer)
-def get_stock_ask(stock_info):
-    return stock_info['ask']
-
-# Bid price (buyer offer)
-def get_stock_bid(stock_info):
-    return stock_info['bid']
 
 # Bid-ask spread = ask - bid
 def bid_ask_spread(stock_info):
-    return get_stock_ask(stock_info) - get_stock_bid(stock_info)
+    return stock_info['ask'] - stock_info['bid']
+
+
+# is the stock above the 50 day moving average
+def above_fifty_day(fifty, bid):
+    return bid > fifty
+
+
+# is the stock above the 200 day moving average
+def fifty_above_twohun(twohun, fifty):
+    return fifty > twohun
 
 # trading volume
 
 # outstanding shares
 # Share Turnover Ratio = trading volume / average shares
 
-my_stocks = ['msft', 'aapl', 'vray', 'gild', 'sage', 'mdb', 'jpm']
+my_stocks = ['msft', 'aapl', 'gild', 'sage', 'mdb', 'flr', 'ntnx', 'trrdx']
+principal = ['fxnax', 'jcbux', 'mphrx', 'pgblx', 'trrfx', 'trrax', 'trrbx', 
+'trrgx', 'trrhx', 'trrcx', 'trrjx', 'trrdx', 'trrkx', 'trrmx', 'trrnx', 'trrlx',
+'fxaix', 'fcgax', 'peiqx', 'vftnx', 'aredx', 'ggotx', 'jvtnx', 'flmvx', 'fsccx', 
+'vspmx', 'vsmsx', 'rerfx', 'odvyx', 'vtmgx'
+]
 
 for ms in my_stocks:
-    stock_info = get_stock_info(ms)
-    ask = get_stock_ask(stock_info)
-    buy = get_stock_bid(stock_info)
-    ba_spread = bid_ask_spread(get_stock_info(ms))
-    print('The sellers are asking {} for {}'.format(ask, ms))
-    print('The buyers are buying at {} for {}'.format(buy, ms))
-    print('This puts the bid-ask spread at {}'.format(ba_spread))
-    
+    stock_info = None
+    try:
+        stock_info = get_stock_info(ms)
+    except:
+        print("That symbol is not available for analysis at this time.")
+
+    if None != stock_info:
+        name = stock_info['longName']
+        change_52 = stock_info['52WeekChange']
+        two_hundred_average = stock_info['twoHundredDayAverage']
+        fifty_average = stock_info['fiftyDayAverage']
+        ask = stock_info['ask']
+        buy = stock_info['bid']
+        ba_spread = bid_ask_spread(get_stock_info(ms))
+        
+        print('\n', name)
+        print('The sellers are asking {} for {}'.format(ask, ms))
+        print('The buyers are buying at {} for {}'.format(buy, ms))
+        print('This puts the bid-ask spread at {}'.format(ba_spread))
+        
+        if above_fifty_day(fifty_average, buy) and fifty_above_twohun(two_hundred_average, fifty_average):
+            print("The current buy price is above the fifty day average and the fifty is above the two hundred.")
+        else:
+            print("The fifty day average is {}".format(fifty_average))
+            print("The two hundred day average is {}".format(two_hundred_average))
+
 print('got all the data')
